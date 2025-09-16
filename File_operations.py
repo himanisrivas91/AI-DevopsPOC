c18fed1329a0a17658ccf572b756b6fe8a28b585
himanisrivas91 <himanisrivas91@gmail.com>
2025-09-16 08:07:01 +0000

    python basics

---
+++ File_operations.py
@@ -0,0 +1,10 @@
+file = open("/root/aidevops/test.txt" , "w")
+file.writelines("Hi There!")
+
+
+
+
+
+file1 = open("/root/aidevops/test1.txt" , "a")
+file1.writelines("Hello")
+file1.writelines("How are you?")
---
+++ advance_data_types.py
@@ -0,0 +1,5 @@
+dict1 = {"subject": "aidevops" , "mode": "online"}
+for keys,values in dict1.items():
+
+   print(f"Keys are: {dict1.keys()}")
+   print(f"Values are: {dict1.values()}")
